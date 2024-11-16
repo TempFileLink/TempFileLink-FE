1a35ec4c0e0929ab89ae62f909e585ce55674815
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2024-11-16 11:06:20 +0700

    Init project

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import withNuxt from './.nuxt/eslint.config.mjs';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import { includeIgnoreFile } from '@eslint/compat';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, '.gitignore');
+
+export default withNuxt([
+  // Note: https://github.com/nuxt/eslint/issues/518
+  {
+    rules: {
+      'no-unused-expressions': 'off',
+      '@typescript-eslint/no-unused-expressions': 'off',
+    },
+  },
+  eslintConfigPrettier,
+  includeIgnoreFile(gitignorePath),
+]);
