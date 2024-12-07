eb18a5a8ab856449e4645594600e3f622cc98003
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2024-12-07 20:06:39 +0700

    build: add dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', '@nuxt/fonts'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/image',
+    'shadcn-nuxt',
+    '@nuxt/icon',
+    '@vueuse/motion/nuxt',
+  ],
 });
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,44 @@
-import type { Config } from 'tailwindcss';
+import animate from 'tailwindcss-animate';
 
-export default <Partial<Config>>{
-  content: [],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ['class'],
+  content: [
+    './pages/**/*.{ts,tsx,vue}',
+    './components/**/*.{ts,tsx,vue}',
+    './app/**/*.{ts,tsx,vue}',
+    './src/**/*.{ts,tsx,vue}',
+  ],
+  prefix: '',
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
-      fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
+      keyframes: {
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+      colors: {
+        primary: '#2953ff',
+        'primary-hover': '#2042cc',
+        'primary-dark': '#04075c',
       },
     },
   },
-  plugins: [],
+  plugins: [animate],
 };
